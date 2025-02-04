09fe99a2175f4be8ea679288a86fb7af1bcc81b7
123047077 <123047077@upq.edu.mx>
2025-02-04 14:34:18 -0600

    Problema 1. Programa Letras Orden. Examen 1P

---
+++ Examen 1P/P1 E1/src/Main.java
@@ -0,0 +1,16 @@
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        /*Escribir un programa que visualice en pantalla los números multiplos de 10 comprendidos entre 10 y 1000*/
+
+                for (int i = 10; i <= 1000; i += 10) {
+
+                    System.out.println("Número: " + i);
+
+                }
+            }
+        }
+
+
---
+++ Examen 1P/P2 E1/src/Main.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+       /* Programa que solicite una palabra y que la descomponga en letras ejemplo: IVAN
+        Resultado: Letra 1: I Letra 2: V Letra 3: A Letra 4: N */
+
+        Scanner sc = new Scanner(System.in);
+
+                System.out.print("INTRODUCE UNA PALABRA: ");
+                String palabra = sc.nextLine();
+
+                for (int i = 0; i < palabra.length(); i++) {
+                    System.out.println("Letra " + (i + 1) + ": " + palabra.charAt(i));
+                }
+
+            }
+        }
+
